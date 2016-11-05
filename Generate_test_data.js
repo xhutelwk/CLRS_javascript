e7983b9a5ee067d5cbc28308a22f8d63210ef559
xhute <xhutelwk@gmail.com>
2016-11-05 12:39:51 +0800

    update generate test data

--- Generate_test_data.js
+++ Generate_test_data.js
@@ -1,10 +1,12 @@
+// {"type":"sort","min":-99999,"max":99999,"quantity":10000}
+
 function Generate_test_data(config){
-    if( 'array' == config.type ){ // random intger array
+    if( "sort" == config.type ){ // random intger array
         var max = config.max;
         var min = config.min;
         var quantity = config.quantity;
 
-        var data = array(quantity);
+        var data = new Array(quantity);
         var scale = max-min;
         for( i=0 ; i<quantity ; i++ ){
             var d = parseInt( Math.random()*scale + min );
